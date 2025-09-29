ca2f10b0e6a643cc6a14f7f452cdf13a454feded
ajit-divakaran <107934595+ajit-divakaran@users.noreply.github.com>
2025-09-30 01:30:49 +0530

    Update serverUrl.js

--- src/services/serverUrl.js
+++ src/services/serverUrl.js
@@ -1,3 +1,3 @@
 
-export const serverUrl = 'https://3s7tcy98v2.execute-api.ap-south-1.amazonaws.com/Firstapideploy'
+export const serverUrl = 'https://3s7tcy98v2.execute-api.ap-south-1.amazonaws.com/deploy2corsenabled'
 
