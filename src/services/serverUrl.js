346d140970793e0510bf0bfa3c2c0a62eaf29480
ajit-divakaran <107934595+ajit-divakaran@users.noreply.github.com>
2025-09-30 10:53:07 +0530

    Integrated API gateway for the server

--- src/services/serverUrl.js
+++ src/services/serverUrl.js
@@ -1,3 +1,3 @@
 
-export const serverUrl = 'https://3s7tcy98v2.execute-api.ap-south-1.amazonaws.com/deploy2corsenabled'
+export const serverUrl = 'https://u2a45nqe6e.execute-api.ap-south-1.amazonaws.com'
 
